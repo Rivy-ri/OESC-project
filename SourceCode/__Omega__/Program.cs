023a1af8aea7b07d9efa1ed8d63abeec597973a5
Adel <118567491+Rivy-ri@users.noreply.github.com>
2023-04-18 17:59:30 +0200

    Add files via upload

---
+++ SourceCode/__Omega__/Program.cs
@@ -0,0 +1,19 @@
+using OmegaSportExplorerClub;
+
+namespace __Omega__
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(LogIn.GetInstance());
+        }
+    }
+}
